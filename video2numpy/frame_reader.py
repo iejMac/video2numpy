8cf0b5fb430ed9330434e4e37c2d9eecd2c9e276
iejMac <kilianmaciej6@gmail.com>
2022-07-13 13:06:39 +0000

    working ffmpeg reader

--- video2numpy/frame_reader.py
+++ video2numpy/frame_reader.py
@@ -3,8 +3,7 @@ import numpy as np
 
 from multiprocessing import shared_memory, SimpleQueue, Process
 
-# from .read_vids_cv2 import read_vids
-from .read_vids_ffmpeg import read_vids
+from .read_vids_cv2 import read_vids
 
 
 class FrameReader:
@@ -30,8 +29,7 @@ class FrameReader:
                          iteration. This means the returned frame block or any slices
                          of it won't work in iterations following the one where it was returned.
                          If you plan on using it out of the iteration set this to False and
-                         remember to keep a reference to the array and manually deallocate it
-                         by calling release_memory once you're done.
+                         remember to manually deallocate it by calling release_memory once you're done.
         """
         self.auto_release = auto_release
         self.info_q = SimpleQueue()
