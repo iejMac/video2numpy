cb40858b63b8ad9af4c4e971a8d0165307959438
iejMac <kilianmaciej6@gmail.com>
2022-11-07 08:27:26 +0000

    done

--- video2numpy/utils.py
+++ video2numpy/utils.py
@@ -10,7 +10,6 @@ QUALITY = "360p"
 def handle_youtube(youtube_url):
     """returns file and destination name from youtube url."""
     ydl_opts = {}
-    print("Trying yt_dlp")
     ydl = yt_dlp.YoutubeDL(ydl_opts)
     info = ydl.extract_info(youtube_url, download=False)
     formats = info.get("formats", None)
